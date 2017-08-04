2a71f7730e3bcce361d57c2da8b461fdb426b444
Igor Grin <garik7012@gmail.com>
2017-08-04 11:37:50 +0300

    changed menu: removed bugs/defects

--- app/Http/Middleware/ShowSideMenu.php
+++ app/Http/Middleware/ShowSideMenu.php
@@ -2,16 +2,11 @@
 
 namespace App\Http\Middleware;
 
-use App\UsersAndRoles;
 use Closure;
 use Auth;
-use App\Enterprise;
 use \Illuminate\Support\Facades\View;
 use App\Menu;
-use App\Action;
-use App\Controller;
-use App\Module;
-use App\RolesAndActions;
+use Illuminate\Support\Facades\DB;
 
 class ShowSideMenu
 {
@@ -32,35 +27,37 @@ class ShowSideMenu
         if(Auth::user()->is_superadmin){
             $menu_items = Menu::orderBy('position')->where('is_active', 1)->get();
         } else {
-            //Prevent repetition of the menu item;
-            $menu_items_id = [];
-
-            $menu_items = Menu::where('is_for_all_users', 1)->orderBy('position')->get();
-            foreach ($menu_items as $menu_item){
-                if($menu_item->parent_id and !in_array($menu_item->parent_id, $menu_items_id)){
-                    $menu_items->prepend(Menu::find($menu_item->parent_id));
-                    $menu_items_id[] = $menu_item->parent_id;
-                }
-                $menu_items_id[] = $menu_item->id;
+            $menu_items = DB::table('users_and_roles')->where('users_and_roles.user_id', '=', Auth::user()->id)
+                ->join('roles', 'roles.id', '=', 'users_and_roles.role_id')->where('roles.is_active', 1)
+                ->join('roles_and_actions', 'roles_and_actions.role_id', '=', 'roles.id')
+                ->join('actions', 'actions.id', '=', 'roles_and_actions.action_id')->where('actions.is_active', 1)
+                ->join('menu', function($join)
+                {
+                    $join->on('menu.action_id', '=', 'actions.id')
+                        ->orWhere('menu.is_for_all_users', '=', '1');
+                })
+                ->where('menu.is_active', 1)
+                ->distinct()
+                ->select('menu.id', 'menu.parent_id')
+                ->get();
+            //we have array collections. make array of assoc arrays
+            // $menu_items = collect($menu_items)->map(function($x){ return (array) $x; })->toArray();
+            //all menu id we will show;
+            $menu_id = [];
+            foreach ($menu_items as $menuItem){
+                $menu_id[] = $menuItem->id;
+                if(!$menuItem->parent_id) continue;
+                $menu_id[] = $menuItem->parent_id;
             }
-            //TODO if role is not active. Sort menu list order by position
-            $user_roles_ids = UsersAndRoles::where('user_id', Auth::user()->id)->select('role_id')->get()->toArray();
-            foreach ($user_roles_ids as $role_id){
-                $actions = RolesAndActions::where('role_id',$role_id['role_id'])->select('action_id')->get();
-                foreach ($actions as $action){
-                    $menu_item = Menu::where('action_id', $action->action_id)->where('is_active',1)->first();
-                    if($menu_item) {
-                        if(in_array($menu_item->id, $menu_items_id)) continue;
-                        if($menu_item->parent_id and !in_array($menu_item->parent_id, $menu_items_id)){
-                            $menu_items->prepend(Menu::find($menu_item->parent_id));
-                            $menu_items_id[] = $menu_item->parent_id;
-                        }
-                        $menu_items->prepend($menu_item);
-                        $menu_items_id[] = $menu_item->id;
-                    }
-                }
+            $menu_items = Menu::whereIn('id', $menu_id)
+                ->orderBy('position')
+                ->get();
+            //if was no roles of user we have empty array. But we need to show is_for_all_users;
+            if(!count($menu_items)){
+                $menu_items = Menu::where('is_active', 1)->where('is_for_all_users',1)->get();
             }
         }
+
         $this->createMenuLinks($menu_items);
         View::share(['menu_items'=>$menu_items]);
         return $next($request);
@@ -71,10 +68,12 @@ class ShowSideMenu
     {
         foreach ($menu_items as &$menuItem){
             if($menuItem->action_id != false) {
-                 $action =  Action::where('id', $menuItem->action_id)->first();
-                 $controller = Controller::where('id', $action->controller_id)->first();
-                 $module = Module::where('id', $controller->module_id)->value('name');
-                 $link = '/'. $module . '/' . $controller->name . '/' . $action->name;
+                $link_raw = DB::table('actions')->where('actions.id', $menuItem->action_id)
+                    ->join('controllers', 'controllers.id', '=', 'actions.controller_id')
+                    ->join('modules', 'modules.id', '=', 'controllers.module_id')
+                    ->select('actions.name as action', 'controllers.name as controller', 'modules.name as module')
+                    ->first();
+                 $link = '/'. $link_raw->module . '/' . $link_raw->controller . '/' . $link_raw->action;
                 $menuItem->link = $link;
             }
             else{
